04e4bb8f9ef7ca419bfd2b9f8aa841e50c660ce8
Chris McKenna <chris.mediathread@gmail.com>
2015-04-15 14:31:37 -0400

    ui.mask removed

--- app/js/app.js
+++ app/js/app.js
@@ -1,5 +1,5 @@
 
 "use strict";
 // Declaramos la aplicacion y definimos sus dependencias
-var MDTCRMApp = angular.module('AngStarter', ['ngRoute','MDTCRMCtrls', 'ngCookies','ui.mask']);
+var MDTCRMApp = angular.module('AngStarter', ['ngRoute','MDTCRMCtrls', 'ngCookies']);
 var MDTCRMCtrls = angular.module('MDTCRMCtrls',[]);
