99972aea9a0e1bd19cf88cfc5c9a6d2beb9a5b9e
Ellen <ellen051394@gmail.com>
2015-04-27 20:50:14 +0800

    fix . . data srvc

--- app/js/Services/dataSvc.js
+++ app/js/Services/dataSvc.js
@@ -4,71 +4,72 @@ angular.module('AngStarter')
        .service('dataSvc', ['dataShare', '$cookieStore','FIREBASEMDTBACKOFFICEDB', function(dataShare, $cookieStore,FIREBASEMDTBACKOFFICEDB) {
             var FIREBASEDB = FIREBASEMDTBACKOFFICEDB;
             return {
-                childlookup: function childlookup (id,recordid,cb,db) {
-                    // var authdata = $cookieStore.get('UserSession');
+                childlookup: function childlookup (id, recordid, cb,db) {
+                    var authdata = $cookieStore.get('UserSession');
                     var fbUrl = FIREBASEDB + db + '/' + id + "/" + recordid;
                     var companyRef = new Firebase(fbUrl);
-                    // companyRef.authWithCustomToken(authdata.token, function(error, authdata) {
-                    //     if (!error) {
+                    console.log(fbUrl);
+                    companyRef.authWithCustomToken(authdata.token, function(error, authdata) {
+                        if (!error) {
                             companyRef.on('value', function (snapshot) {
                                 cb.call(this, snapshot.val());
                             });
-                    //     }
-                    // });
+                        }
+                    });
                 },
                 mdtAddress: function mdtAddress (db,id,cb) {
-                    // var authdata = $cookieStore.get('UserSession');
+                    var authdata = $cookieStore.get('UserSession');
                     var fbUrl = FIREBASEDB + db + '/' + id ;
                     console.log(fbUrl);
                     var companyRef = new Firebase(fbUrl);
-                    // companyRef.authWithCustomToken(authdata.token, function(error, authdata) {
-                    //     if (!error) {
+                    companyRef.authWithCustomToken(authdata.token, function(error, authdata) {
+                        if (!error) {
                             companyRef.on('value', function (snapshot) {
                                 console.log(snapshot.val());
                                 cb.call(this, snapshot.val());
                             });
-                    //     }
-                    // });
+                        }
+                    });
                 },
 
                 getsamplein: function getsamplein (id, recordid, cb, db) {
-                    // var authdata = $cookieStore.get('UserSession');
+                    var authdata = $cookieStore.get('UserSession');
                     var fbUrl = FIREBASEDB + db + '/' + id + "/" + recordid;
                     var companyRef = new Firebase(fbUrl);
-                    // companyRef.authWithCustomToken(authdata.token, function(error, authdata) {
-                    //     if (!error) {
+                    companyRef.authWithCustomToken(authdata.token, function(error, authdata) {
+                        if (!error) {
                            companyRef.on('value', function (snapshot) {
                                 cb.call(this, snapshot.val());
                             });
-                    //      }
-                    // });
+                         }
+                    });
                 },
 
                 getColor: function getColor (cb, db) {
-                    // var authdata = $cookieStore.get('UserSession');
+                    var authdata = $cookieStore.get('UserSession');
                     var fbUrl = FIREBASEDB + 'defaults/' + db ;
                     var companyRef = new Firebase(fbUrl);
-                    // companyRef.authWithCustomToken(authdata.token, function(error, authdata) {
-                    //     if (!error) {
+                    companyRef.authWithCustomToken(authdata.token, function(error, authdata) {
+                        if (!error) {
                            companyRef.on('value', function (snapshot) {
                                 cb.call(this, snapshot.val());
                             });
-                    //      }
-                    // });
+                         }
+                    });
                 },
 
                 getAddress: function getAddress (cb, db) {
-                    // var authdata = $cookieStore.get('UserSession');
+                    var authdata = $cookieStore.get('UserSession');
                     var fbUrl = FIREBASEDB + 'defaults/MDTAddress' ;
                     console.log(fbUrl);
                     var companyRef = new Firebase(fbUrl);
-                    // companyRef.authWithCustomToken(authdata.token, function(error, authdata) {
-                    //     if (!error) {
+                    companyRef.authWithCustomToken(authdata.token, function(error, authdata) {
+                        if (!error) {
                            companyRef.on('value', function (snapshot) {
                                 cb.call(this, snapshot.val());
                             });
-                         // } else { console.log("Error Reading Address!");}
-                    // });
+                         } else { console.log("Error Reading Address!");}
+                    });
                 }
             };
     }])
